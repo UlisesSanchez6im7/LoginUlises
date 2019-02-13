fdff2d5c934188bf00d2a570400ee99df8bd1958
Leesu S <ulimer320@gmail.com>
2019-02-12 21:07:00 -0600

    first commit

---
+++ ReservaU/app/src/main/java/com/example/reservau/Main2Activity.java
@@ -0,0 +1,48 @@
+package com.example.reservau;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Main2Activity extends Activity {
+
+    String nombre = "", fecha = "", hora = "", paterno = "",materno = "",telefono="";
+    int personas = 0;
+    TextView muestraDatos;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.actividad2);
+
+        muestraDatos = (TextView) findViewById(R.id.muestraDatos);
+
+        Bundle recibe = new Bundle();
+        recibe = this.getIntent().getExtras();
+
+        nombre = recibe.getString("nombre");
+        paterno = recibe.getString("paterno");
+        materno = recibe.getString("materno");
+        telefono = recibe.getString("telefono");
+        personas = recibe.getInt("personas");
+        fecha = recibe.getString("fecha");
+        hora = recibe.getString("hora");
+
+        muestraDatos.setText("Reservacion a nombre de:\n" + nombre + " " + paterno + " " + materno + "\n" + "Teléfono: " + telefono +
+                "\n" + personas + " personas\nFecha: " + fecha + "\nHora: " + hora + "\n");
+
+    }
+
+    public void hacerOtraReserva(View v) {
+        Intent envia = new Intent(this, MainActivity.class);
+        finish();
+        startActivity(envia);
+    }
+
+}
