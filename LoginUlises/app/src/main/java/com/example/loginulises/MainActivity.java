0ca211cc01606a828bb11f62657aebd5b89b890b
Leesu S <ulimer320@gmail.com>
2019-02-12 20:47:46 -0600

    fist commit

---
+++ LoginUlises/app/src/main/java/com/example/loginulises/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.loginulises;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.CardView;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button siguiente;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        siguiente=(Button) findViewById(R.id.boton);
+
+        siguiente.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent siguiente=new Intent(MainActivity.this, Main2Activity.class);
+                startActivity(siguiente);
+            }
+        });
+    }
+}
